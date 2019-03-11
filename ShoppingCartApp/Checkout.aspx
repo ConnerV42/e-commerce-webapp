d93ba4dc285abb4332b61727c8ee6f75e3b0eafa
ConnerVerret <cverret@eagles.ewu.edu>
2019-03-11 10:22:58 -0700

    Add Email confirmation upon checkout

--- ShoppingCartApp/Checkout.aspx
+++ ShoppingCartApp/Checkout.aspx
@@ -5,42 +5,42 @@
     <div class="container" runat="server">
         <div class="jumbotron" runat="server">
             <h1 class="display-4">Order Summary</h1>
-            <h6 id="Price" runat="server"></h6>
-            <h6 id="Shipping" runat="server"></h6>
-            <h6 id="Total" runat="server"></h6>
-            <h6 id="Quantity" runat="server"></h6>
-            <h6 id="Weight" runat="server"></h6>
-            <h6 id="Items" runat="server"></h6>
+            <h6 id="PriceDisplay" runat="server"></h6>
+            <h6 id="ShippingDisplay" runat="server"></h6>
+            <h6 id="TotalDisplay" runat="server"></h6>
+            <h6 id="QuantityDisplay" runat="server"></h6>
+            <h6 id="WeightDisplay" runat="server"></h6>
+            <h6 id="ItemsDisplay" runat="server"></h6>
         </div>
         <div class="alert alert-warning alert-dismissible fade show" role="alert"></div>
         <form id="form1" runat="server">
             <div class="form-group">
                 <label for="Name">Name</label>
-                <input class="form-control" type="text" id="Name" runat="server" required="required"/>
+                <input class="form-control" type="text" id="NameInput" runat="server" required="required"/>
             </div>
             <div class="form-group">
                 <label for="Street">Shipping Address</label>
-                <input class="form-control" ID="Street" runat="server" required="required"/>
+                <input class="form-control" ID="StreetInput" runat="server" required="required"/>
             </div>
             <div class="form-group">
                 <label for="City">City</label>
-                <input class="form-control" ID="City" runat="server" required="required"/>
+                <input class="form-control" ID="CityInput" runat="server" required="required"/>
             </div>
             <div class="form-group">
                 <label for="State">State</label>
-                <input class="form-control" ID="State" runat="server" required="required"/>
+                <input class="form-control" ID="StateInput" runat="server" required="required"/>
             </div>
             <div class="form-group">
                 <label for="Zip">Zip Code</label>
-                <input class="form-control" ID="Zip" runat="server" required="required"/>
+                <input class="form-control" ID="ZipInput" runat="server" required="required"/>
             </div>
             <div class="form-group">
                 <label for="Year">Year</label>
-                <input class="form-control" ID="Year" runat="server" required="required"/>
+                <input class="form-control" ID="YearInput" runat="server" required="required"/>
             </div>
             <div class="form-group">
                 <label for="Email">Email Address</label>
-                <input class="form-control" ID="Email" runat="server" required="required"/>
+                <input class="form-control" ID="EmailInput" runat="server" required="required"/>
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <asp:Button class="btn btn-outline-primary" runat="server" OnClick="SubmitOrder_Click" OnClientClick="return validateForm();" Text="Confirm Purchase"/>
--- ShoppingCartApp/Checkout.aspx.cs
+++ ShoppingCartApp/Checkout.aspx.cs
@@ -2,43 +2,70 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Checkout : System.Web.UI.Page
 {
+    private double Price { get; set; }
+    private double Shipping { get; set; }
+    private double Weight { get; set; }
+    private int Quantity { get; set; }
+    private int Items { get; set; }
     protected void Page_Load(object sender, EventArgs e)
     {
-        double price = 0, shippingCost = 0, totalCost = 0, weight = 0, items = 0, quantity = 0;
+        Price = 0;
+        Shipping = 0;
+        Weight = 0;
+        Items = 0;
+        Quantity = 0;
         if(Request.QueryString["Price"] != null)
         {
-            price = double.Parse(Request.QueryString["Price"]);
+            Price = double.Parse(Request.QueryString["Price"]);
         }
         if(Request.QueryString["Weight"] != null)
         {
-            weight = double.Parse(Request.QueryString["Weight"]);
+            Weight = double.Parse(Request.QueryString["Weight"]);
         }
         if (Request.QueryString["Quantity"] != null)
         {
-            quantity = double.Parse(Request.QueryString["Quantity"]);
+            Quantity = int.Parse(Request.QueryString["Quantity"]);
         }
         if (Request.QueryString["Item"] != null)
         {
-            items = double.Parse(Request.QueryString["Item"]);
+            Items = int.Parse(Request.QueryString["Item"]);
         }
-        shippingCost = weight * .42;
-        totalCost = price + shippingCost;
-        Price.InnerText = "Order Cost: " + price.ToString("C", CultureInfo.CurrentCulture);
-        Shipping.InnerText = "Shipping Cost: " + shippingCost.ToString("C", CultureInfo.CurrentCulture);
-        Total.InnerText = "Cost + Shipping: " + totalCost.ToString("C", CultureInfo.CurrentCulture);
-        Weight.InnerText = "Weight: " + weight + " ounces";
-        Quantity.InnerText = "Quantity: " + quantity;
-        Items.InnerText = "Unique Items: " + items;
+        Shipping = Weight * .42;
+        PriceDisplay.InnerText = "Order Cost: " + Price.ToString("C", CultureInfo.CurrentCulture);
+        ShippingDisplay.InnerText = "Shipping Cost: " + Shipping.ToString("C", CultureInfo.CurrentCulture);
+        TotalDisplay.InnerText = "Cost + Shipping: " + (Price + Shipping).ToString("C", CultureInfo.CurrentCulture);
+        WeightDisplay.InnerText = "Weight: " + Weight + " ounces";
+        QuantityDisplay.InnerText = "Quantity: " + Quantity;
+        ItemsDisplay.InnerText = "Unique Items: " + Items;
     }
 
     protected void SubmitOrder_Click(object sender, EventArgs e)
     {
+        try
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.To.Add(EmailInput.Value);
+            mailMessage.From = new MailAddress("postmaster@shop.connercodes.com");
+            mailMessage.Subject = "Sneaker Corner Order Confirmation";
+            mailMessage.Body = "Thank you for your purchase! Your order is being processed, and you will receive an additional email when it has shipped.";
+            mailMessage.Body += "--- Order Info ---\nOrder Cost: $" + Price + "\nShipping Cost: $" + Shipping + "\nTotal Cost: $" + (Price + Shipping) + "\nWeight: " + Weight + "\nQuantity: " + Quantity + "\nUnique Items: " + Items;
+            SmtpClient smtpClient = new SmtpClient("mail.shop.connercodes.com");
+            NetworkCredential Credentials = new NetworkCredential("postmaster@shop.connercodes.com", "hobblewobblejobbletrobblenobblekobble");
+            smtpClient.Credentials = Credentials;
+            smtpClient.Send(mailMessage);
+        }
+        catch (Exception ex)
+        {
+            Response.Write("Could not send the e-mail - error: " + ex.Message);
+        }
         Server.Transfer("OrderSuccess.aspx");
     }
 
